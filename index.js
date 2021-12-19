0b269e5d62dab3ec4236aa73c9fbded98d108a07
Preet P. Vadaliya <preetvadaliya.ict18@gmail.com>
2021-12-19 23:50:11 +0530

    response messages updated

--- index.js
+++ index.js
@@ -8,21 +8,21 @@ module.exports = (app) => {
 
   app.on("issues.opened", async (context) => {
     const issueComment = context.issue({
-      body: `Hey @${context.payload.issue.user.login} Thanks for making an issue, I hope an admin gets it as soon as possible.\n-Quantonium Baby\n-Created By. @preetvadaliya`,
+      body: `Hey @${context.payload.issue.user.login} Thanks for making an issue, I hope an admin gets it as soon as possible.\n\n\nQuantonium Baby\nCreated By. @preetvadaliya`,
     });
     return context.octokit.issues.createComment(issueComment);
   });
 
   app.on("issues.assigned", async (context) => {
     const issueComment = context.issue({
-      body: `Hey @${context.payload.issue.assignee.login} Best of Luck for your future work.\n-Quantonium Baby\n-Created By. @preetvadaliya`,
+      body: `Hey @${context.payload.issue.assignee.login} Best of Luck for your future work.\n\n\nQuantonium Baby\nCreated By. @preetvadaliya`,
     });
     return context.octokit.issues.createComment(issueComment);
   });
 
   app.on("pull_request.opened", async (context) => {
     const pullComment = context.pullRequest({
-      body: `Hey @${context.payload.pull_request.user} Thanks for making a pull request, I hope an admin gets it as soon as possible.\n-Quantonium Baby\n-Created By. @preetvadaliya`,
+      body: `Hey @${context.payload.pull_request.user} Thanks for making a pull request, I hope an admin gets it as soon as possible.\n\n\nQuantonium Baby\nCreated By. @preetvadaliya`,
     });
     return context.octokit.pulls.createComment(pullComment);
   });
